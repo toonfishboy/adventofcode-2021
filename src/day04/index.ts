da34e663c01a44f7ba98235a788b8883f9f7060b
erics <eric.stampa@t-online.de>
2021-12-04 16:25:10 +0100

    day04 preparation

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -1,10 +1,16 @@
 import {getFileLines} from "../helper";
 
 const executeDay = async () => {
-    //const example1 = await calculatePowerConsumption(__dirname + "/example.txt");
-    //const input1 = await calculatePowerConsumption(__dirname + "/input.txt");
+    const example1 = await findBingoBoard(__dirname + "/example.txt");
+    //const input1 = await findBingoBoard(__dirname + "/input.txt");
     //const example2 = await calculateLifeSupportRating(__dirname + "/example.txt");
     //const input2 = await calculateLifeSupportRating(__dirname + "/input.txt");
+    console.log(example1);
+};
+
+export const findBingoBoard = async (filePath: string) => {
+    const lines = await getFileLines(filePath);
+    console.log(lines);
 };
 
 executeDay();
