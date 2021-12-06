5c94be82fffd50edcb95b3f8ebaad2f3f2b3af0e
erics <eric.stampa@t-online.de>
2021-12-06 22:37:04 +0100

    Improve day06

--- src/day06/index.ts
+++ src/day06/index.ts
@@ -2,35 +2,41 @@ import {getFileLines} from "../helper";
 import {join} from "path";
 
 const executeDay = async () => {
-    const example1 = await getLanternFishes(__dirname + "/example.txt", 18);
-    //const input1 = await getLanternFishes(__dirname + "/input.txt", 80);
-    //const example2 = await getLanternFishes(__dirname + "/example.txt", 256);
+    const example1 = await getLanternFishes(__dirname + "/example.txt", 80);
+    const input1 = await getLanternFishes(__dirname + "/input.txt", 80);
+    const example2 = await getLanternFishes(__dirname + "/example.txt", 256);
     //const input2 = await findOverlappingPoints(__dirname + "/input.txt", true);
-    console.log({example1});
+    console.log({example1, input1, example2});
 };
 
 const getLanternFishes = async (filePath: string, days: number) => {
-    const fishes = (await getFileLines(filePath, (line) => line.split(",").map(value => parseInt(value))))[0];
-    const bigFishes: number[][] = [fishes];
+    const allFishes = (await getFileLines(filePath, (line) => line.split(",").map(value => parseInt(value))))[0];
+    const bigFishes: number[][] = [allFishes];
 
     for (let i = 0; i < days; i++) {
         const bigFishesLength = bigFishes.length;
         for (let j = 0; j < bigFishesLength; j++) {
-            const fishLength = fishes.length;
+            const fishLength = bigFishes[j]?.length ?? 0;
+            const fishes = bigFishes[j] ?? [];
             for (let fishIndex = 0; fishIndex < fishLength; fishIndex++) {
                 if (fishes[fishIndex] > 0) fishes[fishIndex] -= 1;
                 else {
                     fishes[fishIndex] = 6;
-                    fishes.push(8);
+                    if (fishLength >= 1000000) {
+                        if (!bigFishes[i])
+                            bigFishes[i] = [8];
+                        else
+                            bigFishes[i].push(8);
+                    } else
+                        fishes.push(8);
                 }
             }
         }
     }
 
     let fishCount = BigInt(0);
-    for (const fishes of bigFishes) {
-        fishCount += BigInt(fishes.length);
-    }
+    for (const fishes of bigFishes)
+        fishCount += BigInt(fishes?.length ?? 0);
 
     return fishCount;
 };
