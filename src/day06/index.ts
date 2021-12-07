791c6026a527401ff8f08c18fda58bef0ea4a851
Eric Stampa <Eric.Stampa@scisys.com>
2021-12-07 09:31:55 +0100

    finish day06

--- src/day06/index.ts
+++ src/day06/index.ts
@@ -1,25 +1,23 @@
 import {getFileLines} from "../helper";
 
 const executeDay = async () => {
-    const example1 = await getLanternFishes(__dirname + "/example.txt", 18);
-    //const input1 = await getLanternFishes(__dirname + "/input.txt", 80);
-    //const example2 = await getLanternFishes(__dirname + "/example.txt", 256);
-    //const input2 = await findOverlappingPoints(__dirname + "/input.txt", true);
-    console.log({example1});
+    const example1 = await getLanternFishes(__dirname + "/example.txt", 80);
+    const input1 = await getLanternFishes(__dirname + "/input.txt", 80);
+    const example2 = await getLanternFishes(__dirname + "/example.txt", 256);
+    const input2 = await getLanternFishes(__dirname + "/input.txt", 256);
+    console.log({example1, input1, example2, input2});
 };
 
 const getLanternFishes = async (filePath: string, days: number) => {
     const fishesAll = (await getFileLines(filePath, (line) => line.split(",").map(value => parseInt(value))))[0];
     const fishes: bigint[] = [];
     fishesAll.forEach(fish => fishes[fish] ? fishes[fish] += BigInt(1) : fishes[fish] = BigInt(1));
-    console.log(0, fishes.join());
     for (let i = 0; i < days; i++) {
-        if (fishes[0]) {
-            fishes[8] ? fishes[8] += BigInt(fishes[0]) : fishes[8] = BigInt(fishes[0])
-            fishes[6] ? fishes[6] += BigInt(fishes[0]) : fishes[6] = BigInt(fishes[0])
+        const newFishes = fishes.shift();
+        if (newFishes) {
+            fishes[8] ? fishes[8] += BigInt(newFishes) : fishes[8] = BigInt(newFishes)
+            fishes[6] ? fishes[6] += BigInt(newFishes) : fishes[6] = BigInt(newFishes)
         }
-        fishes.shift();
-        console.log(i + 1, fishes.join());
     }
     return fishes.reduce((result, value) => result + (value ?? BigInt(0)), BigInt(0));
 };
