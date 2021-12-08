fe58b52a52ea1cede19174f2279cec6f76e9564b
erics <eric.stampa@t-online.de>
2021-12-08 20:21:56 +0100

    danke lurs

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,25 @@ import {writeFile, mkdir, copyFile} from 'fs/promises';
 import {existsSync} from "fs";
 
 const importModule = (directory: string) => {
-    return import(join(__dirname, directory, 'index.ts'));
+    return import(join(directory, 'index.ts'));
 }
 
 (async () => {
     const day = getCurrentDay();
-    if (!existsSync(join(__dirname, `day${day}`))) {
-        await mkdir(join(__dirname, `day${day}`));
-        await copyFile(join(__dirname, "templates/index.ts"), join(__dirname, `day${day}`, "index.ts"))
-        await writeFile(join(__dirname, `day${day}`, "example.txt"), "");
-        await writeFile(join(__dirname, `day${day}`, "input.txt"), "");
+    const dir = join(__dirname, `day${day}`);
+    if (!existsSync(dir)) {
+        await mkdir(dir);
+        await copyFile(join(__dirname, "templates/index.ts"), join(dir, "index.ts"))
+        await writeFile(join(dir, "example.txt"), "");
+        await writeFile(join(dir, "input.txt"), "");
     }
-    const module = await importModule(`day${day}`)
+    const module = await importModule(dir)
     if (module.hasOwnProperty('executePart1')) {
-        console.log({example1: (await module.executePart1(__dirname + `/day${day}/example.txt`))});
-        console.log({input1: (await module.executePart1(__dirname + `/day${day}/input.txt`))})
+        console.log({example1: (await module.executePart1(dir + `/example.txt`))});
+        console.log({input1: (await module.executePart1(dir + `/input.txt`))})
     }
     if (module.hasOwnProperty('executePart2')) {
-        console.log({example2: (await module.executePart2(__dirname + `/day${day}/example.txt`))});
-        console.log({input2: (await module.executePart2(__dirname + `/day${day}/input.txt`))})
+        console.log({example2: (await module.executePart2(dir + `/example.txt`))});
+        console.log({input2: (await module.executePart2(dir + `/input.txt`))})
     }
 })();
