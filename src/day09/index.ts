3d1638b4c6e6b5aab364c40dc3824354becf4500
erics <eric.stampa@t-online.de>
2021-12-09 19:17:52 +0100

    fail day09

--- src/day09/index.ts
+++ src/day09/index.ts
@@ -9,31 +9,34 @@ export const executePart1 = async (filePath: string) => {
 export const executePart2 = async (filePath: string) => {
     const lines = await getFileLines(filePath, (line) => line.split("").map(value => parseInt(value)));
     const lowPoints = getLowPoints(lines);
-    const basinLengths = lowPoints.map(point => findBasins(lines, point, []).length).sort((a, b) => a - b);
+    const basinLengths = lowPoints.map(point => {
+        const basins: number[][] = [];
+        findBasins(lines, point, basins);
+        return basins.length;
+    }).sort((a, b) => a + b);
     console.log(basinLengths);
 };
 
 const findBasins = (lines: number[][], point: number[], basins: number[][]) => {
     const [x, y] = point;
-    const currentBasins: number[][] = [];
-    const isAlreadyInBasin = ([x, y]: number[]) => basins.find(point => {
-        return x === point[0] && point[1] === y
-    });
-    const isBasin = (value: number | undefined) => !!value && value !== 9;
-    if (isAlreadyInBasin([x, y - 1]) && isBasin(lines[x][y - 1]))
-        currentBasins.push(...findBasins(lines, [x, y - 1], [...basins, [x, y - 1]]));
-    if (isAlreadyInBasin([x, y + 1]) && isBasin(lines[x][y - 1]))
-        currentBasins.push(...findBasins(lines, [x, y + 1], [...basins, [x, y - 1]]));
-    if (isAlreadyInBasin([x - 1, y]) && isBasin(lines[x - 1][y]))
-        currentBasins.push(...findBasins(lines, [x - 1, y], [...basins, [x, y - 1]]));
-    if (isAlreadyInBasin([x + 1, y]) && isBasin(lines[x + 1][y]))
-        currentBasins.push(...findBasins(lines, [x + 1, y], [...basins, [x, y - 1]]));
+    const hasPoint = (x: number, y: number) => basins.find(point => point[0] === x && point[1] === y);
+    const isBasin = (x: number, y: number) => x >= 0 && y >= 0 && !hasPoint(x, y) && lines?.[x]?.[y] !== undefined && lines[x][y] !== 9;
+    const beginLength = basins.length;
 
-    const allBasins = [...currentBasins, ...basins];
+    if (isBasin(x - 1, y))
+        basins.push([x - 1, y]);
+    if (isBasin(x + 1, y))
+        basins.push([x + 1, y]);
+    if (isBasin(x, y - 1))
+        basins.push([x, y - 1]);
+    if (isBasin(x, y + 1))
+        basins.push([x, y + 1]);
 
-    return [...currentBasins, ...basins].filter((basin, index, arr) => {
-        return index === arr.findIndex(value => value[0] === basin[0] && value[1] === basin[1]);
-    });
+    const baseBasins = [...basins];
+    if (beginLength === baseBasins.length) return;
+    for (let index = 0; index < baseBasins.length; index++) {
+        findBasins(lines, baseBasins[index], baseBasins);
+    }
 };
 
 const getLowPoints = (lines: number[][]) => {
